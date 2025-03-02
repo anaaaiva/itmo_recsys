7a4a04bb8bb0434a9f6aea8880abf017dda5c85a
anaaaiva <ivawillow.a@gmail.com>
2025-03-02 16:54:11 +0300

    trying to fix 401 in github actions

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -8,13 +8,20 @@ from service.settings import ServiceConfig
 
 GET_RECO_PATH = "/reco/{model_name}/{user_id}"
 
-load_dotenv()
+if not os.getenv("PERSONAL_TOKEN"):
+    load_dotenv()
+
+PERSONAL_TOKEN = os.getenv("PERSONAL_TOKEN")
+
+
+def test_auth():
+    assert PERSONAL_TOKEN is not None, "PERSONAL_TOKEN не задан"
 
 
 def test_health(client: TestClient) -> None:
-    print(f'PERSONAL_TOKEN: {os.getenv("PERSONAL_TOKEN")}')
+    print(f"PERSONAL_TOKEN: {PERSONAL_TOKEN}")
     with client:
-        response = client.get("/health", headers={"Authorization": f'Bearer {os.getenv("PERSONAL_TOKEN")}'})
+        response = client.get("/health", headers={"Authorization": f"Bearer {PERSONAL_TOKEN}"})
     assert response.status_code == HTTPStatus.OK
 
 
@@ -22,11 +29,11 @@ def test_get_reco_success(
     client: TestClient,
     service_config: ServiceConfig,
 ) -> None:
-    print(f'PERSONAL_TOKEN: {os.getenv("PERSONAL_TOKEN")}')
+    print(f"PERSONAL_TOKEN: {PERSONAL_TOKEN}")
     user_id = 123
     path = GET_RECO_PATH.format(model_name="range_model", user_id=user_id)
     with client:
-        response = client.get(path, headers={"Authorization": f'Bearer {os.getenv("PERSONAL_TOKEN")}'})
+        response = client.get(path, headers={"Authorization": f"Bearer {PERSONAL_TOKEN}"})
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -37,37 +44,35 @@ def test_get_reco_success(
 def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
-    print(f'PERSONAL_TOKEN: {os.getenv("PERSONAL_TOKEN")}')
+    print(f"PERSONAL_TOKEN: {PERSONAL_TOKEN}")
     user_id = 10**10
     path = GET_RECO_PATH.format(model_name="range_model", user_id=user_id)
     with client:
-        response = client.get(path, headers={"Authorization": f'Bearer {os.getenv("PERSONAL_TOKEN")}'})
+        response = client.get(path, headers={"Authorization": f"Bearer {PERSONAL_TOKEN}"})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
 
 
 def test_model_not_found_error(client: TestClient) -> None:
-    print(f'PERSONAL_TOKEN: {os.getenv("PERSONAL_TOKEN")}')
-    personal_token = os.getenv("PERSONAL_TOKEN")
+    print(f"PERSONAL_TOKEN: {PERSONAL_TOKEN}")
     incorrect_path = GET_RECO_PATH.format(model_name="BeSt_MoDeL_iN_tHe_WoRlD", user_id=123)
 
     correct_path = GET_RECO_PATH.format(model_name="range_model", user_id=123)
 
     with client:
-        bad_response = client.get(incorrect_path, headers={"Authorization": f"Bearer {personal_token}"})
-        good_response = client.get(correct_path, headers={"Authorization": f"Bearer {personal_token}"})
+        bad_response = client.get(incorrect_path, headers={"Authorization": f"Bearer {PERSONAL_TOKEN}"})
+        good_response = client.get(correct_path, headers={"Authorization": f"Bearer {PERSONAL_TOKEN}"})
 
     assert good_response.status_code == HTTPStatus.OK
     assert bad_response.status_code == HTTPStatus.NOT_FOUND
 
 
 def test_authorization_with_token(client: TestClient) -> None:
-    print(f'PERSONAL_TOKEN: {os.getenv("PERSONAL_TOKEN")}')
-    personal_token = os.getenv("PERSONAL_TOKEN")
+    print(f"PERSONAL_TOKEN: {PERSONAL_TOKEN}")
     invalid_token = "INVALIDTOKEN"
 
     with client:
-        good_response = client.get("/reco/range_model/123", headers={"Authorization": f"Bearer {personal_token}"})
+        good_response = client.get("/reco/range_model/123", headers={"Authorization": f"Bearer {PERSONAL_TOKEN}"})
 
         bad_response = client.get("/reco/range_model/123", headers={"Authorization": f"Bearer {invalid_token}"})
 
